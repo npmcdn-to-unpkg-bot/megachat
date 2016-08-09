da737661e9d2e0e9ac01e31aa0070011244e7ab3
Mega <Aza.Noriega@gmail.com>
2016-08-09 10:54:52 +0500

    add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+const path = require('path');
+module.exports = {
+	entry: './src/client/entry.js',
+	output: {
+		path: path.resolve(__dirname, 'public', 'js'),
+		publicPath: 'public/',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [{
+			test: /.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2015', 'react']
+			}
+		}]
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'public')
+	}
+};
