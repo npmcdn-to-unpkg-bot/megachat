7422dc2e5f9e3a0a4c4133f6169474b281f85ee4
Mega <Aza.Noriega@gmail.com>
2016-08-09 08:56:47 +0500

    rebootOnSignal false, for nodemon compatibility

--- server.js
+++ server.js
@@ -2,34 +2,35 @@ var argv = require('minimist')(process.argv.slice(2));
 var SocketCluster = require('socketcluster').SocketCluster;
 
 var options = {
-  workers: Number(argv.w) || Number(process.env.SOCKETCLUSTER_WORKERS) || 1,
-  brokers: Number(argv.b) || Number(process.env.SOCKETCLUSTER_BROKERS) || 1,
-  port: Number(argv.p) || Number(process.env.SOCKETCLUSTER_PORT) || 8000,
-  // If your system doesn't support 'uws', you can switch to 'ws' (which is slower but works on older systems).
-  wsEngine: process.env.SOCKETCLUSTER_WS_ENGINE || 'uws',
-  appName: argv.n || process.env.SOCKETCLUSTER_APP_NAME || null,
-  workerController: argv.wc || process.env.SOCKETCLUSTER_WORKER_CONTROLLER || __dirname + '/worker.js',
-  brokerController: argv.bc || process.env.SOCKETCLUSTER_BROKER_CONTROLLER || __dirname + '/broker.js',
-  socketChannelLimit: Number(process.env.SOCKETCLUSTER_SOCKET_CHANNEL_LIMIT) || 1000,
-  clusterStateServerHost: argv.cssh || process.env.SCC_STATE_SERVER_HOST || null,
-  clusterStateServerPort: process.env.SCC_STATE_SERVER_PORT || null,
-  clusterAuthKey: process.env.SCC_AUTH_KEY || null,
-  clusterStateServerConnectTimeout: Number(process.env.SCC_STATE_SERVER_CONNECT_TIMEOUT) || null,
-  clusterStateServerAckTimeout: Number(process.env.SCC_STATE_SERVER_ACK_TIMEOUT) || null,
-  clusterStateServerReconnectRandomness: Number(process.env.SCC_STATE_SERVER_RECONNECT_RANDOMNESS) || null,
-  crashWorkerOnError: argv['auto-reboot'] != false
+	rebootOnSignal: false,
+	workers: Number(argv.w) || Number(process.env.SOCKETCLUSTER_WORKERS) || 1,
+	brokers: Number(argv.b) || Number(process.env.SOCKETCLUSTER_BROKERS) || 1,
+	port: Number(argv.p) || Number(process.env.SOCKETCLUSTER_PORT) || 8000,
+	// If your system doesn't support 'uws', you can switch to 'ws' (which is slower but works on older systems).
+	wsEngine: process.env.SOCKETCLUSTER_WS_ENGINE || 'uws',
+	appName: argv.n || process.env.SOCKETCLUSTER_APP_NAME || null,
+	workerController: argv.wc || process.env.SOCKETCLUSTER_WORKER_CONTROLLER || __dirname + '/worker.js',
+	brokerController: argv.bc || process.env.SOCKETCLUSTER_BROKER_CONTROLLER || __dirname + '/broker.js',
+	socketChannelLimit: Number(process.env.SOCKETCLUSTER_SOCKET_CHANNEL_LIMIT) || 1000,
+	clusterStateServerHost: argv.cssh || process.env.SCC_STATE_SERVER_HOST || null,
+	clusterStateServerPort: process.env.SCC_STATE_SERVER_PORT || null,
+	clusterAuthKey: process.env.SCC_AUTH_KEY || null,
+	clusterStateServerConnectTimeout: Number(process.env.SCC_STATE_SERVER_CONNECT_TIMEOUT) || null,
+	clusterStateServerAckTimeout: Number(process.env.SCC_STATE_SERVER_ACK_TIMEOUT) || null,
+	clusterStateServerReconnectRandomness: Number(process.env.SCC_STATE_SERVER_RECONNECT_RANDOMNESS) || null,
+	crashWorkerOnError: argv['auto-reboot'] != false
 };
 
 var SOCKETCLUSTER_OPTIONS;
 
 if (process.env.SOCKETCLUSTER_OPTIONS) {
-  SOCKETCLUSTER_OPTIONS = JSON.parse(process.env.SOCKETCLUSTER_OPTIONS);
+	SOCKETCLUSTER_OPTIONS = JSON.parse(process.env.SOCKETCLUSTER_OPTIONS);
 }
 
 for (var i in SOCKETCLUSTER_OPTIONS) {
-  if (SOCKETCLUSTER_OPTIONS.hasOwnProperty(i)) {
-    options[i] = SOCKETCLUSTER_OPTIONS[i];
-  }
+	if (SOCKETCLUSTER_OPTIONS.hasOwnProperty(i)) {
+		options[i] = SOCKETCLUSTER_OPTIONS[i];
+	}
 }
 
 var socketCluster = new SocketCluster(options);
